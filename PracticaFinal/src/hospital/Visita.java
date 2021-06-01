41ddc4b9a1bebf4b914529a416a680ca9499ff4a
cesarwalls <cesar.paredes.lopez@outlook.com>
2021-06-01 22:31:38 +0200

    asdaws

--- PracticaFinal/src/hospital/Visita.java
+++ PracticaFinal/src/hospital/Visita.java
@@ -13,30 +13,22 @@ import java.util.Random;
  *
  * @author cesar
  */
-public class Visitas {
+public class Visita {
+
     protected int identificador;
     protected Fecha fecha;
+    protected ArrayList<Medico> medicos;
     protected int IDMedico;
+    protected ArrayList<Paciente> pacientes;
     protected int IDPaciente;
     protected int contador = 0;
-    protected Hospital hospital = new Hospital();
 
-    
-
-    public Visitas() {
+    public Visita() {
         contador++;
         identificador = contador;
         fecha = new Fecha();
-        IDMedico= hospital.personal.get((int) (Math.random()*hospital.personal.size())).getID();
-        ArrayList<Paciente> pacientes = new ArrayList(hospital.pacientes);
-        IDPaciente= pacientes.get((int) (Math.random()*hospital.personal.size())).getID();   
-        }
-
-    public Visitas(int identificador, Fecha fecha, int IDMedico, int IDPaciente) {
-        this.identificador = identificador;
-        this.fecha = fecha;
-        this.IDMedico = IDMedico;
-        this.IDPaciente = IDPaciente;
+        IDMedico = medicos.get((int) (Math.random() * medicos.size())).getID();
+        IDPaciente = pacientes.get((int) (Math.random() * medicos.size())).getID();
     }
 
     public int getIdentificador() {
@@ -79,31 +71,9 @@ public class Visitas {
         this.contador = contador;
     }
 
-    public Hospital getHospital() {
-        return hospital;
-    }
-
-    public void setHospital(Hospital hospital) {
-        this.hospital = hospital;
-    }
-
     @Override
     public String toString() {
-        return "Visitas{" + "identificador=" + identificador + ", fecha=" + fecha + ", IDMedico=" + IDMedico + ", IDPaciente=" + IDPaciente + ", contador=" + contador + ", hospital=" + hospital + '}';
+        return "Visita{" + "identificador=" + identificador + ", fecha=" + fecha + ", IDMedico=" + IDMedico + ", IDPaciente=" + IDPaciente + '}';
     }
-    
-    
-    
-    
-    
-    
-    }
-    
 
-    
-    
-    
-    
-    
-    
-
+}
